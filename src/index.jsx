035f1ec05bb3f9854d828e6d2d30037f5a2797ea
Husain Champawala <husainchampawala@gmail.com>
2024-03-17 16:44:46 +0530

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ToastContainer, Zoom } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId="459718214021-84fgkbjcaj4c44ir0eua9rku2sbjj9k5.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+    <ToastContainer transition={Zoom} />
+  </React.StrictMode>
+);
---
+++ src/utils/Api.jsx
@@ -0,0 +1,3 @@
+// export const api = "https://project-management-app-4kjw.onrender.com";
+// export const api = "http://localhost:8879";
+export const api = "https://major-project-pmt-backend.onrender.com";
